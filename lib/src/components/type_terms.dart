e1954a5547294f2904fed7a4120e1c56e812e373
Josue Silva de Morais <josuemorais@gmail.com>
2023-05-29 10:19:50 -0300

    Correção no termo de consentimento

--- lib/src/components/type_terms.dart
+++ lib/src/components/type_terms.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import '../modules/main/parameters.dart';
 import 'header_card.dart';
 
@@ -13,10 +14,32 @@ class TypeTerms extends StatefulWidget {
 }
 
 class _TypeTermsState extends State<TypeTerms> {
+  final _formKey = GlobalKey<FormState>();
+  final textController = TextEditingController();
+  final focusNode = FocusNode();
+  bool checkBoxValue = false;
   @override
   void initState() {
     super.initState();
     widget.answer.value = {};
+  }
+
+  _senderMessage() {
+    if (_formKey.currentState?.validate() ?? false) {
+      //Faça aqui o codigo com a mensagem final message = textController.text;
+      textController.text = '';
+    }
+    focusNode.requestFocus();
+  }
+
+  _changeComboBox(bool? newValue) {
+    newValue ??= false;
+    if (newValue) {
+      widget.answer.value = {'aceito': true};
+    } else {
+      widget.answer.value = {};
+    }
+    setState(() => checkBoxValue = newValue!);
   }
 
   @override
@@ -31,34 +54,89 @@ class _TypeTermsState extends State<TypeTerms> {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           Text(
-            telas[widget.id]!['body'],
+            telas[widget.id]!['body1'],
             textAlign: TextAlign.justify,
           ),
           const SizedBox(height: 10.0),
-          const Divider(),
-          Row(crossAxisAlignment: CrossAxisAlignment.start, children: [
-            Material(
-              color: Colors.lightGreenAccent.shade100,
-              child: Checkbox(
-                  value: widget.answer.value['aceito'] != null,
-                  onChanged: (value) {
-                    setState(() {
-                      if (value == true) {
-                        widget.answer.value = {'aceito': true};
-                      } else {
-                        widget.answer.value = {};
-                      }
-                    });
-                  }),
-            ),
-            const Padding(
-              padding: EdgeInsets.only(left: 10, top: 0, right: 0, bottom: 0),
-              child: Text(
-                'Eu li e aceito os termos e condições',
-                style: TextStyle(height: 2),
+          CheckboxListTile(
+            title: const Text('Concordo'),
+            value: checkBoxValue,
+            onChanged: _changeComboBox,
+          ),
+          const SizedBox(height: 10.0),
+          CheckboxListTile(
+              title: const Text('Não concordo'),
+              value: !checkBoxValue,
+              onChanged: (newValue) {
+                newValue ??= false;
+                _changeComboBox(!newValue);
+              }),
+          const SizedBox(height: 10.0),
+          Text(
+            telas[widget.id]!['body2'],
+            textAlign: TextAlign.justify,
+          ),
+          const SizedBox(height: 10.0),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Form(
+              key: _formKey,
+              autovalidateMode: AutovalidateMode.always,
+              child: TextFormField(
+                keyboardType: TextInputType.emailAddress,
+                inputFormatters: [
+                  FilteringTextInputFormatter.deny(
+                      RegExp('[\\s]')), // Remove espaços em branco
+                  FilteringTextInputFormatter.deny(RegExp(
+                      r'[^\w\s@\.-]')), // Remove caracteres especiais, exceto @, . e -
+                ],
+                focusNode: focusNode,
+                controller: textController,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return null;
+                  }
+                  if (!RegExp(r'^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$')
+                      .hasMatch(value)) {
+                    return 'Por favor, insira um email válido.';
+                  }
+                  return null;
+                },
+                decoration: InputDecoration(
+                  border: const OutlineInputBorder(),
+                  fillColor: Colors.white,
+                  filled: true,
+                  labelText: 'Seu e-mail',
+                  hintText: 'Seu e-mail',
+                  suffixIcon: AnimatedBuilder(
+                    animation: textController,
+                    builder: (context, _) {
+                      return IconButton(
+                        onPressed: _senderMessage,
+                        icon: const Icon(Icons.send),
+                      );
+                    },
+                  ),
+                ),
               ),
             ),
-          ]),
+          ),
+          const Divider(),
+          const SizedBox(height: 10.0),
+          Text(
+            telas[widget.id]!['body3'],
+            textAlign: TextAlign.justify,
+          ),
+          const SizedBox(height: 10.0),
+          Image.asset(
+            telas[widget.id]!['image'][0], //assets/arvore2.png
+            alignment: Alignment.bottomCenter,
+          ),
+          const SizedBox(height: 10.0),
+          Image.asset(
+            telas[widget.id]!['image'][1], //assets/arvore2.png
+            alignment: Alignment.bottomCenter,
+          ),
         ],
       ),
     );
